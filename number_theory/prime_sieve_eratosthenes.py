af871eefa5964d14996ed7ef28249ed1db095d44
eanutt1272.v2 <eanutt1272.v2@gmail.com>
2026-07-13 16:37:33 +0100

    perf(number_theory): refactor prime sieve to use Casio-native matplotlib subset

--- number_theory/prime_sieve_eratosthenes.py
+++ number_theory/prime_sieve_eratosthenes.py
@@ -1,6 +1,6 @@
-# Generate primes with the Sieve of Eratosthenes.
+# Generate and plot prime distribution on Casio fx-CG100.
 
-from matplotlib.pyplot import axis, bar, grid, show, text
+from matplotlib.pyplot import axis, plot, show
 import math
 
 try:
@@ -12,6 +12,7 @@ except ImportError:
 def sieve(n):
     is_p = [True] * (n + 1)
     is_p[0] = is_p[1] = False
+    
     for i in range(2, int(math.sqrt(n)) + 1):
         if is_p[i]:
             for j in range(i * i, n + 1, i):
@@ -23,20 +24,30 @@ def wait_for_exit():
     if getkey is not None:
         getkey()
     else:
-        input("\nPress any key to exit: ")
+        input("\nPress EXE to exit: ")
 
 
 def main():
-    N = 200
+    N = 250
     primes = sieve(N)
-    print("Count:", len(primes))
-    print("Largest:", primes[-1])
+    
+    print("Total Primes:", len(primes))
+    print("Max Prime:", primes[-1])
 
-    xs = list(range(len(primes)))
-    bar(xs, primes)
-    axis([0, len(primes), 0, N])
-    grid("on", "grey")
-    text(N / 3, N - 15, "Primes up to " + str(N))
+    axis([0, N, 0, len(primes) + 2])
+
+    plot_local = plot
+    current_count = 0
+    prev_x = 0
+
+    for p in primes:
+        plot_local([prev_x, p], [current_count, current_count])
+        current_count += 1
+        plot_local([p, p], [current_count - 1, current_count])
+        prev_x = p
+
+    plot_local([prev_x, N], [current_count, current_count])
+
     show()
     wait_for_exit()
 
