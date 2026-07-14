3d9b969cab0f00fb9c24b3bea698e853eb2333ca
eanutt1272.v2 <eanutt1272.v2@gmail.com>
2026-07-14 17:21:49 +0100

    style(psi_fx): replace cmap_data with cmap_registry for colour maps

--- physics/psi_fx.py
+++ physics/psi_fx.py
@@ -2,7 +2,7 @@ from casioplot import clear_screen, draw_string, set_pixel, show_screen
 from math import log, exp, pi, sqrt, sin, cos
 
 from psi_fx_lib import (
-    cmap_data,
+    cmap_registry,
     cmap,
     fmt_density,
     read_float,
@@ -60,19 +60,19 @@ unit_choice = read_int(
 )
 
 print("colour maps:")
-for i in range(len(cmap_data)):
-    print(str(i + 1) + ": " + cmap_data[i])
+for i in range(len(cmap_registry)):
+    print(str(i + 1) + ": " + cmap_registry[i])
 cm_idx = (
     read_int(
-        "select (1-" + str(len(cmap_data)) + ", default=6): ",
+        "select (1-" + str(len(cmap_registry)) + ", default=6): ",
         min_value=1,
-        max_value=len(cmap_data),
+        max_value=len(cmap_registry),
         default=6,
     )
     - 1
 )
 
-cm_name, RC, GC, BC = cmap_data[cm_idx]
+cm_name, RC, GC, BC = cmap_registry[cm_idx]
 
 if R <= 0.0:
     inner_term = n * n - l * (l + 1)
